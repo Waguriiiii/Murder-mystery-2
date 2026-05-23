135ec41be491201506f502a66194f893bb2963d6
Waguriiiii <saltygraham88@gmail.com>
2026-05-23 14:08:19 +0800

    Create Waguri-mmv-bomb-jump.lua

---
+++ Waguri-mmv-bomb-jump.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/197516f63d21e75cde9940bee93a678b14019472afc652fb7063c940e269e731/download"))()
