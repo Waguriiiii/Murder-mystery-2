335ff37673affed8e0be7d4c22330aa77b332877
Waguriiiii <saltygraham88@gmail.com>
2026-06-03 13:06:39 +0800

    Update Waguri-MMV.lua

--- Waguri-MMV.lua
+++ Waguri-MMV.lua
@@ -1,2 +1,2 @@
 getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/efff34ca2a2574cdb3cb3f12d63fbe177d6cd80162f1b61dbc1d2c47cb0e38ae/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/80100391b604a2747a2151423fcfdd81f382c09446ec8a1723a2844abab2b218/download"))()
